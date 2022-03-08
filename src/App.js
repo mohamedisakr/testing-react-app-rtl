e18fa4c8634eb78165b4ee2334e6ab023db926ce
mohamedisakr <mohamedisakr@gmail.com>
2022-03-08 14:21:29 +0200

    testing click event

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
+import {useState} from 'react'
 import './App.css'
 
 function App() {
+  const [buttonColor, setButtonColor] = useState('red')
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red'
+
   const handleClick = event => {
     event.preventDefault()
-    event.target.style = "backgroundColor: 'green'"
+    setButtonColor(newButtonColor)
   }
 
   return (
     <div className="App">
       <h2>Testing React using Jest and React Testing Library</h2>
-      <button onClick={handleClick} style={{backgroundColor: 'red'}}>
-        Change to blue
+      <button onClick={handleClick} style={{backgroundColor: buttonColor}}>
+        Change to {newButtonColor}
       </button>
     </div>
   )
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,5 @@
 import {render, screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
 test('button has initail color', () => {
@@ -7,4 +8,11 @@ test('button has initail color', () => {
   expect(button).toHaveStyle({backgroundColor: 'red'})
 })
 
-test('button color turns to blue when clicked', () => {})
+test('button color turns to blue when clicked', () => {
+  render(<App />)
+  const button = screen.getByRole('button', {name: /change/i})
+  expect(button).toHaveStyle({backgroundColor: 'red'})
+  userEvent.click(button)
+  expect(button).toHaveStyle({backgroundColor: 'blue'})
+  expect(button).toHaveTextContent(/red/i)
+})
