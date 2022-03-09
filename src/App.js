321f0bf29b1ebaf2bb95a7bb374af59d0cfa03c4
mohamedisakr <mohamedisakr@gmail.com>
2022-03-09 08:31:42 +0200

    configuring prettier

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import './App.css'
-import {camelToCapitalWithSpaces} from './utils/string'
+import { camelToCapitalWithSpaces } from './utils/string'
 
 function App() {
   // MediumVioletRed --> MidnightBlue
